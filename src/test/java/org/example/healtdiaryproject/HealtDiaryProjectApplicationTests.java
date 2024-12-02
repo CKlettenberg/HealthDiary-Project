72eacf4a82eab5c891a0dd0e305bbba9884cd789
opilane <klettenberg@gmail.com>
2024-12-02 10:39:38 +0200

    First

---
+++ src/test/java/org/example/healtdiaryproject/HealtDiaryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.healtdiaryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealtDiaryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
