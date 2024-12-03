de30188c278723422f077618ac00844297df007f
opilane <klettenberg@gmail.com>
2024-12-03 09:04:46 +0200

    First

--- src/main/java/org/example/healthdiaryproject/config/SecurityConfig.java
+++ src/main/java/org/example/healthdiaryproject/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package org.example.healthdiaryproject.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SecurityConfig {
