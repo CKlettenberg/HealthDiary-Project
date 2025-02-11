7ca4cbd8417f65cb2d7d439b73f2d34bda019dbe
CKlettenberg <klettenberg@gmail.com>
2025-02-11 10:17:38 +0200

    Update CorsConfig.java

--- src/main/java/org/example/healthdiaryproject/config/CorsConfig.java
+++ src/main/java/org/example/healthdiaryproject/config/CorsConfig.java
@@ -3,15 +3,15 @@ package org.example.healthdiaryproject.config;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
+
 public class CorsConfig {
-    //@Bean
+    
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") // Applies to all endpoints
-                        .allowedOrigins("*") // Vue frontend
+                registry.addMapping("/**") 
+                        .allowedOrigins("*") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
